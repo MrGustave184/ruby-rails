0a683464c2560c61a9e69f0ee33ad0d7b6a7885b
Rafael Ortiz <rafael.ortiz@mybrightwheel.com>
2025-01-23 19:36:54 -0400

    puts, p and print

---
+++ echo.rb
@@ -0,0 +1,9 @@
+# put returns null, prints new line at end
+puts "hello world"
+
+# p (shortcut) returns the string, prints new line at end
+p "hello world"
+
+# print returns null, does not print new line at end
+print "hello world"
+print "hello world \n"
